eb248272eadd7a6e747a414107e9cdf06d119a0a
YogananthJ <mjyogananth@gmail.com>
2025-10-03 14:21:04 +0530

    Model improved

--- sim.py
+++
@@ -1,7 +0,0 @@
-import torch
-from ultralytics import YOLO
-import ultralytics.nn.tasks
-
-# Allowlist DetectionModel global
-with torch.serialization.safe_globals([ultralytics.nn.tasks.DetectionModel]):
-    model = YOLO("fire_smoke_yolov8_densenet.pt")  # full model
